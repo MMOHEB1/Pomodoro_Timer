682e1bf464ea2efb9887a4e7dc152b748f64c563
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-05-02 10:51:48 +0100

    commit 6: Finished project, although not complete. I found a bug where if you repeatedly click the start button, the title/label overlap each other rather than replacing the old title. also the tick only appears once after the cycles of the timer is complete.

--- main.py
+++ main.py
@@ -11,38 +11,61 @@ WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 REPS = 1
+timer = NONE
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+
+def reset_timer():
+    window.after_cancel()
+    canvas.itemconfig(timer_text, text="00:00")
+    label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 def start_timer():
     global REPS
+    REPS += 1
     work_sec = WORK_MIN * 60
     short_break = SHORT_BREAK_MIN * 60
     long_break = LONG_BREAK_MIN * 60
-    if REPS == 1 or REPS == 3 or REPS == 5 or REPS == 7:
-        count_down(work_sec)
-        REPS += 1
-    elif REPS == 2 or REPS == 4 or REPS == 6:
-        count_down(short_break)
-        REPS += 1
-    elif REPS == 8:
+    if REPS % 8 == 0:
         count_down(long_break)
-        REPS = 1
+        label = Label(text="NOICE! TAKE A 20 MIN BREAK", font=(FONT_NAME, 35, "bold"), fg=RED, highlightthickness=0,
+                      bg=YELLOW)
+        label.grid(column=1, row=0)
+    elif REPS % 2:
+        count_down(short_break)
+        label = Label(text="YOU DESERVE 5 MINS!", font=(FONT_NAME, 35, "bold"), fg=PINK, highlightthickness=0,
+                      bg=YELLOW)
+        label.grid(column=1, row=0)
+    else:
+        count_down(work_sec)
+        label = Label(text="GET TO WORKING", font=(FONT_NAME, 35, "bold"), fg=GREEN, highlightthickness=0, bg=YELLOW)
+        label.grid(column=1, row=0)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 def count_down(count):
-    count_min = math.floor(count/60)
+    count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec < 10:
         count_sec = f"0{count_sec}"
-    canvas.itemconfig(timer, text=f"{count_min}:{count_sec}")
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(REPS/2)
+        for _ in range(work_sessions):
+            marks += "✅"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -56,14 +79,14 @@ label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 img_file = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=img_file)
-timer = canvas.create_text(100, 130, text="25:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="25:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 start_btn = Button(text="Start", highlightthickness=0, bd=0, command=start_timer)
 start_btn.grid(column=0, row=2)
 
-tick_label = Label(text="✅", bg=YELLOW)
-tick_label.grid(column=1, row=3)
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 reset_btn = Button(text="Reset", highlightthickness=0, bd=0)
 reset_btn.grid(column=2, row=2)
